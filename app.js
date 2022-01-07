5d0fe2df9ea50adf4f8c1c03a40636fdd33337c7
kellerflint <kflint0068@gmail.com>
2022-01-06 17:03:09 -0800

    added yargs and command parsing

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 
 const getNotes = require('./notes')
 
-console.log(getNotes())
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.red.bold.inverse('failed'))
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            require: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note text',
+            require: true,
+            type: 'string'
+        },
+    },
+    handler: (argv) => {
+        console.log('Title:', argv.title, '\nBody:', argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: () => {
+        console.log('Remove note...')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: () => {
+        console.log('Listing notes...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: () => {
+        console.log('Reading note...')
+    }
+})
+
+yargs.parse()
